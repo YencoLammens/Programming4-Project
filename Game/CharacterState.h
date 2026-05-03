ca58722c4b11e34c65fcd326494bbb9f44ba59cd
Yenco Lammens <161343595+YencoLammens@users.noreply.github.com>
2026-05-03 05:19:50 +0200

    Preliminary version of states (compiles)

---
+++ Game/CharacterState.h
@@ -0,0 +1,24 @@
+#pragma once
+#include <memory>
+
+namespace dae
+{
+    class GameObject;
+
+    class CharacterState
+    {
+    public:
+        virtual ~CharacterState() = default;
+        CharacterState(const CharacterState&) = delete;
+        CharacterState& operator=(const CharacterState&) = delete;
+        CharacterState(CharacterState&&) = delete;
+        CharacterState& operator=(CharacterState&&) = delete;
+
+        virtual void OnEnter(GameObject* owner) = 0;
+        virtual std::unique_ptr<CharacterState> Update(GameObject* owner, float deltaTime) = 0;
+        virtual void OnExit(GameObject* owner) = 0;
+
+    protected:
+        CharacterState() = default;
+    };
+}
